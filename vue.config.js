00c101ed5ceaaafd4baa1397bd0d43cd4806ef00
Sovidi <lim132445@gmail.com>
2023-11-17 04:01:25 +0900

    sdf

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
-module.exports = {
-  publicPath: "https://sovidi.github.io/portfilio/"
-}
+// module.exports = {
+//   publicPath: "https://sovidi.github.io/portfilio/"
+// }
