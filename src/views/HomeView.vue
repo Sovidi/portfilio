8015c32514116c03564632817f7537eab1dcd30c
Sovidi <lim132445@gmail.com>
2023-11-19 16:23:08 +0900

    wsfed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -167,35 +167,35 @@ export default {
         responsiveWidth: 780,
       },
       skillbox1: [
-        { id: 1, src: "./img/js.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 2, src: "./img/css.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 3, src: "./img/scss.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 4, src: "./img/html.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 5, src: "./img/jquery.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 6, src: "./img/react.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 7, src: "./img/typescript.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 8, src: "./img/next.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 9, src: "./img/vue.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false }
+        { id: 1, src: "./img/js.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 다룹니다.", active: false },
+        { id: 2, src: "./img/css.png", text: "도큐먼트 디자인을 위한 CSS코드를 다룰 수 있습니다.", active: false },
+        { id: 3, src: "./img/scss.png", text: "심화된 CSS코드인 SCSS를 더 주로 다룹니다.", active: false },
+        { id: 4, src: "./img/html.png", text: "도큐먼트 구조파악과 마크업을 위한 기초부터 심화적 구조를 다룹니다.", active: false },
+        { id: 5, src: "./img/jquery.png", text: "자바스크립트에 더해 다룰 줄 아는 가장 기초적인 보조언어입니다.", active: false },
+        { id: 6, src: "./img/react.png", text: "자바스크립트에 대한 이해도와 더불어 데이터교환의 구조를 파악하고 있으며 여러 프로젝트를 수행하였습니다.", active: false },
+        { id: 7, src: "./img/typescript.png", text: "대규모 프로젝트에서 안전성을 구비할 수 있습니다.", active: false },
+        { id: 8, src: "./img/next.png", text: "리엑트에 대한 이해도와 더불어 서버를 다루는데에 특히 이해도가 높습니다.", active: false },
+        { id: 9, src: "./img/vue.png", text: "기본적인 컨텍스트(store) 이해와 더불어 vue의 각종 독립 훅을 이해하고 능동적으로 다룰 수 있습니다.", active: false }
       ],
       skillbox2: [
-        { id: 10, src: "./img/mysql.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 11, src: "./img/express.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 12, src: "./img/npm.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 13, src: "./img/mongodb.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 14, src: "./img/mariadb.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 15, src: "./img/nodejs.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false }
+        { id: 10, src: "./img/mysql.png", text: "express 와 next.js 에서의 프로젝트로 여러 데이터 교환 구조를 만들어 본 경험이 있습니다.", active: false },
+        { id: 11, src: "./img/express.png", text: "서버사이드 렌더링이 내장된 next.js 를 제외한 리액트와 vue 에서 게이트웨이로 다루어 보았습니다.", active: false },
+        { id: 12, src: "./img/npm.png", text: "프레임워크에서 각종 모듈을 위한 기초적인 모듈로써 모든 구조에서 응용하고 있습니다.", active: false },
+        { id: 13, src: "./img/mongodb.png", text: "express 와 더불어 모든 프레임워크에서 주요 명령어를 모듈화시켜 사용하고 있습니다.", active: false },
+        { id: 14, src: "./img/mariadb.png", text: "mysql 과 더불어 같은 구조로써 서버사이드랜더링 구조를 이해하는데 사용하였습니다.", active: false },
+        { id: 15, src: "./img/nodejs.png", text: "프론트단과 서버단의 구조연결을 위한 기초적 구조를 알고있습니다.", active: false }
       ],
       skillbox3: [
-        { id: 16, src: "./img/github.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 17, src: "./img/cloudtype.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 18, src: "./img/postman.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 19, src: "./img/swiper.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 20, src: "./img/fullpage.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 21, src: "./img/figma.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 22, src: "./img/zeplin.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 23, src: "./img/vercel.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 24, src: "./img/netlify.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false },
-        { id: 25, src: "./img/notion.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 알고있어요.", active: false }
+        { id: 16, src: "./img/github.png", text: "각종 프로젝트 버전 공유와 업데이트를 위해 사용할 줄 알고있습니다.", active: false },
+        { id: 17, src: "./img/cloudtype.png", text: "서버사이드렌더링으로 제한적이지만 동적서버를 제공하는 사이트로 몇몇 프로젝트 서버를 위해 사용중입니다.", active: false },
+        { id: 18, src: "./img/postman.png", text: "각종 API들을 사용하기 앞서 사용자 키와 각종 파라미터를 테스트하는데 사용하였습니다.", active: false },
+        { id: 19, src: "./img/swiper.png", text: "페이지 전환이라는 요소에서 횡스크롤 디자인과 기능적으로 가장 이상적인 플러그인 이라고 생각합니다.", active: false },
+        { id: 20, src: "./img/fullpage.png", text: "종스크롤 페이지 전환에서 주로 사용하는 플러그인입니다.", active: false },
+        { id: 21, src: "./img/figma.png", text: "페이지 사전구상에서 가장 기초적으로 사용하고 있으며 기본적인 디자인 구조를 만들 수 있어요.", active: false },
+        { id: 22, src: "./img/zeplin.png", text: "포토샵과 연결하여 기초적인 사이트 디자인을 구상하는데 사용할 수 있어요.", active: false },
+        { id: 23, src: "./img/vercel.png", text: "next.js 프로젝트 빌드와 업로드를 위해 사용중입니다.", active: false },
+        { id: 24, src: "./img/netlify.png", text: "vue 프로젝트 빌드와 업로드를 위해 사용중입니다.", active: false },
+        { id: 25, src: "./img/notion.png", text: "각종 중요한 코드 메모와 팀 프로젝트 상황공유를 위해 사용하고 있으며 마크다운 명령어를 주로 다룰 수 있어요.", active: false }
       ],
       workbox: [
         {id: 1, name: "Wonder place", desc: "어쩌구 저쩌구", src: "./img/wonderplace.png", skills: "#react, #css, #html", href: "https://sovidi.github.io/wonderplace_clone/", githref:"https://github.com/Sovidi/wonderplace_clone" },
@@ -206,15 +206,14 @@ export default {
         {id: 6, name: "petSalon", desc: "어쩌구 저쩌구", src: "./img/petsalon.png", skills: "#react, #css, #html", href: "https://www.naver.com/", githref:"https://www.daum.net/" }
       ],
       contact: [
-        {id: 1, text:"https://velog.io/@qwert4572", address: "https://velog.io/@qwert4572", src: "./img/velog.png"},
-        {id: 2, text:"", address: "", src: "./img/velog.png"},
-        {id: 3, text:"", address: "", src: "./img/velog.png"},
-        {id: 4, text:"", address: "", src: "./img/velog.png"},
-        {id: 5, text:"Instagram", address: "https://www.instagram.com/chaemmni/", src: "./img/velog.png"}
+        {id: 1, text:"010-4338-4358", address: "#page4", src: "./img/phone.png"},
+        {id: 2, text:"lim132445@gmail.com", address: "#page4", src: "./img/mail.png"},
+        {id: 4, text:"https://velog.io/@qwert4572", address: "https://velog.io/@qwert4572", src: "./img/velog.png"},
+        {id: 5, text:"Instagram", address: "https://www.instagram.com/chaemmni/", src: "./img/instagram.png"}
       ],
 
       comment: [],
-      instance : axios.create({baseURL:"http://localhost:3050"}),
+      instance : axios.create({baseURL:"https://port-0-portfolio2-6w1j2alm48bfok.sel5.cloudtype.app"}),
       name: "",
       email: "",
       text: ""
