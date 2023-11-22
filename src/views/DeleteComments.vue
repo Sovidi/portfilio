96d12a797205f1b05ed77d1f3ba8ebdc4b30bfff
Sovidi <lim132445@gmail.com>
2023-11-23 00:05:15 +0900

    Merge branch 'main' of https://github.com/Sovidi/portfilio

---
+++ src/views/DeleteComments.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <button @click="cmDelete">삭제</button>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    methods:{
+        cmDelete() {
+            axios.delete(`http://localhost:3050/api/delete`)
+        }
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
