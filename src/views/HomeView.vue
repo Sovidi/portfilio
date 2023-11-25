672225779fa9d07d8225e56ae6229bad153a9d2b
Sovidi <lim132445@gmail.com>
2023-11-26 02:44:09 +0900

    sdf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -181,7 +181,7 @@ export default {
         afterLoad: this.afterLoad,
         afterResponsive: this.afterResponsive,
         anchors: ['page1', 'page2', 'page3', 'page4'],
-        responsiveWidth: 780,
+        responsiveWidth: 1000,
       },
       skillbox1: [
         { id: 1, src: "./img/js.png", text: "프론트앤드개발을 위한 기본적인 스크립트 구조를 다룹니다.", active: false },
@@ -221,7 +221,7 @@ export default {
         {id: 1, name: "Wonder place", desc: "원더플레이스 브랜드관 소개페이지 클론코딩", src: "./img/wonderplace.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/wonderplace_clone/", githref:"https://github.com/Sovidi/wonderplace_clone" },
         {id: 2, name: "Everland", desc: "에버랜드 홈페이지 리뉴얼", src: "./img/everland.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/everland/", githref:"https://github.com/Sovidi/everland" },
         {id: 3, name: "Scary House", desc: "귀신의 집, 공포 추리 미니 게임", src: "./img/scaryhouse.png", skills: "#react, #scss, #html, #Express", href: "https://sovidi.github.io/scary/", githref:"https://github.com/Sovidi/scary" },
-        {id: 4, name: "Movie Finder", desc: "MovieDB API 를 활용한 간단 무비 파인더", src: "./img/moviefinder.png", skills: "#react, #scss, #html", href: "https://sovidi.github.io/movies/", githref:"https://github.com/Sovidi/movies" },
+        {id: 4, name: "Movie Finder", desc: "MovieDB API 를 활용한 간단 무비 파인더", src: "./img/moviefinder.png", skills: "#react, #scss, #html", href: "https://movies-nine-xi.vercel.app/", githref:"https://github.com/Sovidi/movies" },
         {id: 5, name: "toGrocery", desc: "시장 농산물 물가현황 간편확인 및 위치기반 매칭 어플", src: "./img/togrocery.png", skills: "#next.js, #scss, #html", href: "https://togrocery.vercel.app/", githref:"https://github.com/Sovidi/togrocery" },
         {id: 6, name: "petSalon", desc: "미용사 찾기 위치기반 서비스 어플", src: "./img/petsalon.png", skills: "#react, #css, #html", href: "#", githref:"#" }
       ],
