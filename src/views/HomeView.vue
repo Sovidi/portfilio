c25e1473cedc65fb941ca4c510f4cf37a3fd210c
Sovidi <lim132445@gmail.com>
2023-12-11 02:13:41 +0900

    serverTest

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -221,9 +221,9 @@ export default {
         {id: 1, name: "Wonder place", desc: "원더플레이스 브랜드관 소개페이지 클론코딩", src: "./img/wonderplace.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/wonderplace_clone/", githref:"https://github.com/Sovidi/wonderplace_clone" },
         {id: 2, name: "Everland", desc: "에버랜드 홈페이지 리뉴얼", src: "./img/everland.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/everland/", githref:"https://github.com/Sovidi/everland" },
         {id: 3, name: "Scary House", desc: "귀신의 집, 공포 추리 미니 게임", src: "./img/scaryhouse.png", skills: "#react, #scss, #html, #Express", href: "https://sovidi.github.io/scary/", githref:"https://github.com/Sovidi/scary" },
-        {id: 4, name: "Movie Finder", desc: "MovieDB API 를 활용한 간단 무비 파인더", src: "./img/moviefinder.png", skills: "#react, #scss, #html", href: "https://movies-nine-xi.vercel.app/", githref:"https://github.com/Sovidi/movies" },
+        {id: 4, name: "Movie Finder", desc: "MovieDB API 를 활용한 간단 무비 파인더", src: "./img/moviefinder.png", skills: "#react, #scss, #html", href: "https://sovidi.github.io/movies/", githref:"https://github.com/Sovidi/movies" },
         {id: 5, name: "toGrocery", desc: "시장 농산물 물가현황 간편확인 및 위치기반 매칭 어플", src: "./img/togrocery.png", skills: "#next.js, #scss, #html", href: "https://togrocery.vercel.app/", githref:"https://github.com/Sovidi/togrocery" },
-        {id: 6, name: "petSalon", desc: "미용사 찾기 위치기반 서비스 어플", src: "./img/petsalon.png", skills: "#react, #css, #html", href: "#", githref:"#" }
+        {id: 6, name: "petSalon (현재 개발중)", desc: "미용사 찾기 위치기반 서비스 어플", src: "./img/petsalon.png", skills: "#react, #css, #html", href: "#", githref:"#" }
       ],
       contact: [
         {id: 1, text:"010-4338-4358", address: "#page4", src: "./img/phone.png"},
@@ -234,9 +234,9 @@ export default {
       ],
 
       comment: [],
-      instance : axios.create({baseURL:"https://port-0-portfolio2-6w1j2alm48bfok.sel5.cloudtype.app"}),
+      // instance : axios.create({baseURL:"https://port-0-portfolio2-6w1j2alm48bfok.sel5.cloudtype.app"}),
       // instance : axios.create({baseURL:process.env.E_HOST}),
-      // instance : axios.create({baseURL:"http://localhost:3050/"}),
+      instance : axios.create({baseURL:"http://localhost:3050/"}),
       name: "",
       email: "",
       text: ""
@@ -261,9 +261,9 @@ export default {
 
     afterResponsive: function (isResponsive) {
       if (isResponsive) {
-        this.options.autoScrolling == false; // 768px 이상에서 풀페이지 자동 스크롤 비활성화
+        this.options.autoScrolling == false;
       } else {
-        this.options.autoScrolling == true; // 768px 이상에서 풀페이지 자동 스크롤 비활성화
+        this.options.autoScrolling == true;
       }
     },
 
