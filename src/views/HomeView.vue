e83d038edded9db173efc377a8a88691eb8f55fc
Sovidi <lim132445@gmail.com>
2023-12-13 04:33:17 +0900

    subport

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -216,13 +216,14 @@ export default {
         { id: 27, src: "./img/german.png", text: "독일에서의 유학생활중 학업과 현업으로 독일어 회화가 가능합니다.", active: false }
       ],
       workbox: [
-        {id: 0, name: "Portfolio", desc: "보고계신 포트폴리오 페이지 입니다.", src: "./img/portfolio.gif", skills: "#Vue.js, #scss, #html, #MongoDB, #Express", href: "#", githref:"https://github.com/Sovidi/portfolio2" },
-        {id: 1, name: "Wonder place", desc: "원더플레이스 브랜드관 소개페이지 클론코딩", src: "./img/wonderplace.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/wonderplace_clone/", githref:"https://github.com/Sovidi/wonderplace_clone" },
-        {id: 2, name: "Everland", desc: "에버랜드 홈페이지 리뉴얼", src: "./img/everland.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/everland/", githref:"https://github.com/Sovidi/everland" },
-        {id: 3, name: "Scary House", desc: "귀신의 집, 공포 추리 미니 게임", src: "./img/scaryhouse.png", skills: "#react, #scss, #html, #Express", href: "https://sovidi.github.io/scary/", githref:"https://github.com/Sovidi/scary" },
-        {id: 4, name: "Movie Finder", desc: "MovieDB API 를 활용한 간단 무비 파인더", src: "./img/moviefinder.png", skills: "#react, #scss, #html", href: "https://sovidi.github.io/movies/", githref:"https://github.com/Sovidi/movies" },
-        {id: 5, name: "toGrocery", desc: "시장 농산물 물가현황 간편확인 및 위치기반 매칭 어플", src: "./img/togrocery.png", skills: "#next.js, #scss, #html", href: "https://togrocery.vercel.app/", githref:"https://github.com/Sovidi/togrocery" },
-        {id: 6, name: "petSalon (현재 개발중)", desc: "미용사 찾기 위치기반 서비스 어플", src: "./img/petsalon.png", skills: "#react, #css, #html", href: "#", githref:"#" }
+        {id: 0, name: "Portfolio Vue.js", desc: "Vue.js로 제작된 보고계신 포트폴리오 페이지 입니다.", src: "./img/portfolio.gif", skills: "#Vue.js, #scss, #html, #MongoDB, #Express", href: "#", githref:"https://github.com/Sovidi/portfolio2" },
+        {id: 1, name: "Portfolio Next.js", desc: "포트폴리오의 Next.js 이식버전입니다.", src: "./img/portfolio.gif", skills: "#Next.js, #scss, #html, #MongoDB, #Express", href: "https://portfolio3-blue.vercel.app/", githref:"https://github.com/Sovidi/portfolio3" },
+        {id: 2, name: "Wonder place", desc: "원더플레이스 브랜드관 소개페이지 클론코딩", src: "./img/wonderplace.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/wonderplace_clone/", githref:"https://github.com/Sovidi/wonderplace_clone" },
+        {id: 3, name: "Everland", desc: "에버랜드 홈페이지 리뉴얼", src: "./img/everland.png", skills: "#javascript, #scss, #html, #jQuery", href: "https://sovidi.github.io/everland/", githref:"https://github.com/Sovidi/everland" },
+        {id: 4, name: "Scary House", desc: "귀신의 집, 공포 추리 미니 게임", src: "./img/scaryhouse.png", skills: "#react, #scss, #html, #Express", href: "https://sovidi.github.io/scary/", githref:"https://github.com/Sovidi/scary" },
+        {id: 5, name: "Movie Finder", desc: "MovieDB API 를 활용한 간단 무비 파인더", src: "./img/moviefinder.png", skills: "#react, #scss, #html", href: "https://sovidi.github.io/movies/", githref:"https://github.com/Sovidi/movies" },
+        {id: 6, name: "toGrocery", desc: "시장 농산물 물가현황 간편확인 및 위치기반 매칭 어플", src: "./img/togrocery.png", skills: "#next.js, #scss, #html", href: "https://togrocery.vercel.app/", githref:"https://github.com/Sovidi/togrocery" },
+        {id: 7, name: "petSalon (현재 개발중)", desc: "미용사 찾기 위치기반 서비스 어플", src: "./img/petsalon.png", skills: "#react, #css, #html", href: "#", githref:"#" }
       ],
       contact: [
         {id: 1, text:"010-4338-4358", address: "#page4", src: "./img/phone.png"},
